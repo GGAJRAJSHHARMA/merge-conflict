241823519403094989ecf27adb43d47b01fa8b3d
root <root@ip-172-31-38-119.us-east-2.compute.internal>
2020-06-22 16:59:02 +0000

    First Feature1

--- main.c
+++ main.c
@@ -5,9 +5,18 @@ Initial Function()
   //Sample Code
 }
 
+<<<<<<< HEAD
 Initial Security()
 {
  //Sample Code
+=======
+Initial Security()                                           |  --------------------------------------------------------------|  feature1()
+  {                                                            |  --------------------------------------------------------------|  {
+   //Sample Code                                               |  --------------------------------------------------------------|    //Sample Code for Feature 1
+  }  feature1()
+{
+  //Sample Code for Feature 1
+>>>>>>> feature1
 }
 
 
