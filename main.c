041bd54051a64746f0b5e753fa80756b0c3dd222
root <root@ip-172-31-38-119.us-east-2.compute.internal>
2020-06-22 17:07:18 +0000

    remove junk value

--- main.c
+++ main.c
@@ -1,24 +1,19 @@
 pseudo code
 {
+
 Initial Function()
 {
   //Sample Code
 }
 
-<<<<<<< HEAD
 Initial Security()
 {
  //Sample Code
-=======
-Initial Security()                                           |  --------------------------------------------------------------|  feature1()
-  {                                                            |  --------------------------------------------------------------|  {
-   //Sample Code                                               |  --------------------------------------------------------------|    //Sample Code for Feature 1
-  }  feature1()
-{
-  //Sample Code for Feature 1
->>>>>>> feature1
 }
 
-
+feature1()
+{
+  //Sample Code for Feature 1
+}
 
 }
